3d2439f9aeff441369b2603244ca931056318a02
vmedina <vamedinac@icloud.com>
2021-02-08 21:49:27 -0500

    consultas_con_orm

--- Webapp/api/controllers/view-productos.js
+++ Webapp/api/controllers/view-productos.js
@@ -15,8 +15,28 @@ module.exports = {
   },
 
   fn: async function (inputs, exits) {
+    //asi estamos pidiendo todos los articulos
+    //const productos = await Articulo.find()
+    //asi pedimos un articulo en especifico o igual a marca :pera
+    //const productos = await Articulo.find({marca:"naranja"})
+    //con dos condiciones
+    //const productos = await Articulo.find({marca:"naranja", precio: 25})
+    //MAYOR A 20
+    const productos = await Articulo.find({
+      precio:{
+        '>':20
+      }
+    })
+    //CONSULTA MUCHO MAS AVANZADA
 
-    const productos = await Articulo.find()
+    const productos = await Articulo.find({
+      //que salte unos registros
+      where :{},
+      //limite de reigistros
+      skip:
+      sort:
+    })
+
     // Respond with view.
     return exits.success({productos});
 
